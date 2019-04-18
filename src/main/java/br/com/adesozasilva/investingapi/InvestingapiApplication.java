5dc54706caeacf2bb403e3f42df7e5224bb0caa3
Anderson Silva <adesozasilva@gmail.com>
2019-04-18 20:16:56 -0300

    First commit

---
+++ src/main/java/br/com/adesozasilva/investingapi/InvestingapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.adesozasilva.investingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestingapiApplication.class, args);
+	}
+
+}
